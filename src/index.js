f801f3a65fae4a02bf0df3b0224aa630ce253de8
Fabian-MG <mirandafabian1510@gmail.com>
2020-08-06 09:43:49 -0500

    Finishing client side app, adding components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Router from "./Router";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "antd/dist/antd.css";
+import "./index.css";
+
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+
+import * as serviceWorker from "./serviceWorker";
+
+Amplify.configure(config);
+
+
+ReactDOM.render(<Router />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
